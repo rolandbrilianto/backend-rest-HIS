dbffcb3c2ecf142e9b485c0016a15dbe41658313
Roland Brilianto <rolandbrilianto@gmail.com>
2026-06-05 16:32:25 +0700

    fix(db):change to connectionString

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString,
   ssl: { rejectUnauthorized: false },
 });
 
